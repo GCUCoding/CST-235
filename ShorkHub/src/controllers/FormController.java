dbcbc6c41e9f43f37cdefb2221e389f648f23dc7
Max1mus7 <maxdweber7@gmail.com>
2021-02-20 20:44:08 -0700

    added login/registration with file

--- ShorkHub/src/controllers/FormController.java
+++ ShorkHub/src/controllers/FormController.java
@@ -3,6 +3,11 @@ package controllers;
 import javax.faces.context.FacesContext;
 
 import beans.User;
+import services.UserService;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
@@ -12,17 +17,22 @@ import javax.faces.bean.ViewScoped;
 public class FormController 
 {
 
-	
+	UserService userService;
 	public String onLoginSubmit(User user)
 		{
-
+			userService = new UserService();
 			FacesContext.getCurrentInstance().getExternalContext().getRequestMap().put("user", user);
+			boolean loginSuccess = userService.validateUser(user);
+			FacesContext.getCurrentInstance().getExternalContext().getRequestMap().put("success", loginSuccess);
 			return "LoginResponse.xhtml";
 		}
     public String onRegistrationSubmit(User user)
 	{
 
+    	userService = new UserService();
 		FacesContext.getCurrentInstance().getExternalContext().getRequestMap().put("user", user);
+		boolean successAdded = userService.addUser(user);
+		FacesContext.getCurrentInstance().getExternalContext().getRequestMap().put("success", successAdded);
 		return "RegistrationResponse.xhtml";
 	}
 }
---
+++ ShorkHub/src/services/UserService.java
@@ -0,0 +1,69 @@
+package services;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+import beans.User;
+
+public class UserService
+{
+	public boolean addUser(User user)
+	{
+		try {
+            FileWriter writer = new FileWriter("RegisteredUsers.txt", true);
+            BufferedWriter bw = new BufferedWriter(writer);
+            PrintWriter pw = new PrintWriter(bw);
+            pw.append("{\n");
+            pw.append(user.getName() + "\n");
+            pw.append(user.getId() + "\n");
+            pw.append(user.getEmail() + "\n");
+            pw.append(user.getBirthday() + "\n");
+            pw.append(user.getUsername() + "\n");
+            pw.append(user.getPassword() + "\n");
+            pw.append("}\n");
+            pw.close();
+            return true;
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+	}
+	
+	public boolean validateUser(User user)
+	{
+		try {
+		User checkUser = new User();
+		Scanner fileIn = new Scanner(new File("RegisteredUsers.txt"));
+		String currLine = "";
+		while(fileIn.hasNextLine())
+		{
+			currLine = fileIn.nextLine();
+			if(currLine.equals("{"))
+			{
+				checkUser.setName(fileIn.nextLine());
+				checkUser.setId(Integer.valueOf(fileIn.nextLine()));
+				checkUser.setEmail(fileIn.nextLine());
+				checkUser.setBirthday(fileIn.nextLine());
+				checkUser.setUsername(fileIn.nextLine());
+				checkUser.setPassword(fileIn.nextLine());
+			}
+			if(checkUser.getUsername().equals(user.getUsername()) && user.getPassword().equals(checkUser.getPassword()))
+			{
+				return true;
+			}
+		}
+		return false;
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+			return false;
+		}
+	}
+}
