249398d6eb86025fe89bdf65ce6c7dc2ba46a715
Max1mus7 <pokerimproductions7@gmail.com>
2021-03-07 18:09:43 -0700

    added product/database models and skeleton of products view

---
+++ ShorkHub/src/beans/Database.java
@@ -0,0 +1,37 @@
+package beans;
+
+import java.util.ArrayList;
+
+public class Database
+{
+	private ArrayList<Product> products;
+	private ArrayList<User> users;
+	
+	public Database()
+	{
+		this.products = new ArrayList<Product>();
+		this.users = new ArrayList<User>();
+	}
+
+	public ArrayList<Product> getProducts()
+	{
+		return products;
+	}
+
+	public void setProducts(ArrayList<Product> products)
+	{
+		this.products = products;
+	}
+
+	public ArrayList<User> getUsers()
+	{
+		return users;
+	}
+
+	public void setUsers(ArrayList<User> users)
+	{
+		this.users = users;
+	}
+	
+	
+}
--- ShorkHub/src/controllers/FormController.java
+++ ShorkHub/src/controllers/FormController.java
@@ -1,35 +1,39 @@
 package controllers;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 
 import beans.User;
 import services.UserService;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
-
 @ManagedBean
 @ViewScoped
-public class FormController 
+public class FormController
 {
-
+	
 	UserService userService;
+	
 	public String onLoginSubmit(User user)
+	{
+		userService = new UserService();
+		FacesContext.getCurrentInstance().getExternalContext().getRequestMap().put("user", user);
+		boolean loginSuccess = userService.validateUser(user);
+		FacesContext.getCurrentInstance().getExternalContext().getRequestMap().put("success", loginSuccess);
+		if (loginSuccess)
 		{
-			userService = new UserService();
-			FacesContext.getCurrentInstance().getExternalContext().getRequestMap().put("user", user);
-			boolean loginSuccess = userService.validateUser(user);
-			FacesContext.getCurrentInstance().getExternalContext().getRequestMap().put("success", loginSuccess);
 			return "LoginResponse.xhtml";
 		}
-    public String onRegistrationSubmit(User user)
+		else
+		{
+			return "Login.xhtml";
+		}
+	}
+	
+	public String onRegistrationSubmit(User user)
 	{
-
-    	userService = new UserService();
+		
+		userService = new UserService();
 		FacesContext.getCurrentInstance().getExternalContext().getRequestMap().put("user", user);
 		boolean successAdded = userService.addUser(user);
 		FacesContext.getCurrentInstance().getExternalContext().getRequestMap().put("success", successAdded);
